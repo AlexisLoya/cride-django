4e23eba18d87e63a21b1235d435c713a63796137
AlexisLoya <alexloy117@gmail.com>
2021-05-07 21:50:54 -0500

    Implement Django Rest Framework and serializers

--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,9 +1,25 @@
 """Circles views"""
 
-# Django
-from django.http import HttpResponse
+# Django REST Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import  Response
+# Models
+from cride.circles.models import Circle
+# Serializer
+from cride.circles.serializers import CircleSerializer, CreateCircleSerializer
 
-
+@api_view(['GET'])
 def list_circles(request):
-    """list circles"""
-    return HttpResponse('Hoal')
+    """List circles"""
+    circles = Circle.objects.filter(is_public=True)
+    #data = [CircleSerializer(circle).data for circle in circles]
+    serializer = CircleSerializer(circles, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_circles(request):
+    """Create circle"""
+    serializer = CreateCircleSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    circle = serializer.save()
+    return Response(CircleSerializer(circle).data)
